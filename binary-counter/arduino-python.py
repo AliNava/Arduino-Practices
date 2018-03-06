2155e03cc3d762fdbde00f9343eca5f6d9dcd07d
AliNava <alien.nava.alfaro@gmail.com>
2018-03-05 21:47:17 -0600

    correccionde identacion

--- binary-counter/arduino-python.py
+++ binary-counter/arduino-python.py
@@ -11,18 +11,18 @@ button_pin = 23
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-	i = 0
-	j=0
+    i = 0
+    j=0
     if GPIO.input(button_pin):
-    	for i in range(0,255):
-    		for j in range(0,8):
-    			if ( ( (i >> j) & 1 )  == 1 ):
-    				print((str(pins[j]) + ",1").encode())
-    				#arduino.write((str(pins[j]) + ",1").encode())
-    				GPIO.output(pins[j],1)
-    			else:
-    				print((str(pins[j]) + ",0").encode())
-    				#arduino.write((str(pins[j]) + ",0").encode())
-    				GPIO.output(pins[j],0)
-    			time.sleep(1)
-    		print("--------------     ")
+        for i in range(0,255):
+            for j in range(0,8):
+                if ( ( (i >> j) & 1 )  == 1 ):
+                    print((str(pins[j]) + ",1").encode())
+				    #arduino.write((str(pins[j]) + ",1").encode())
+                    GPIO.output(pins[j],1)
+                else:
+                    print((str(pins[j]) + ",0").encode())
+				    #arduino.write((str(pins[j]) + ",0").encode())
+                    GPIO.output(pins[j],0)
+                time.sleep(1)
+            print("--------------     ")
