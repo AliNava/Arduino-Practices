532be91612cab3dcbb26f8071b04a92e1d2486ff
AliNava <alien.nava.alfaro@gmail.com>
2018-03-05 22:05:50 -0600

    se imprimio en el serial la cadena de pines de estados

--- binary-counter/arduino-python.py
+++ binary-counter/arduino-python.py
@@ -4,9 +4,10 @@ import serial
 
 GPIO.setmode(GPIO.BCM)
 pins=[21,20,16,12,7,8,25,24]
+arduino_pins=[2,3,4,5,6,7,8,9]
 for i in pins:
     GPIO.setup(i, GPIO.OUT, initial=GPIO.LOW)
-#arduino = serial.Serial('/dev/ttyACM0', 2304000)
+arduino = serial.Serial('/dev/ttyACM0', 2304000)
 button_pin = 23
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
@@ -18,13 +19,11 @@ while True:
             serial_string = ""
             for j in range(0,8):
                 if ( ( (i >> j) & 1 )  == 1 ):
-                    print((str(pins[j]) + ",1").encode())
                     GPIO.output(pins[j],1)
-                    serial_string += str(pins[j]) + ",1-"
+                    serial_string += str(arduino_pins[j]) + ",1-"
                 else:
-                    print((str(pins[j]) + ",0").encode())
                     GPIO.output(pins[j],0)
-                    serial_string += str(pins[j]) + ",0-"
-            #arduino.write((str(pins[j]) + ",1").encode())
+                    serial_string += str(arduino_pins[j]) + ",0-"
+            arduino.write(serial_string.encode())
             print serial_string
             time.sleep(1)
