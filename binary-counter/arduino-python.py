8e5d735b5262c6eb7a64202380cac80fd9388b8c
AliNava <alien.nava.alfaro@gmail.com>
2018-03-05 21:27:44 -0600

    se agrego el if para el boton

--- binary-counter/arduino-python.py
+++ binary-counter/arduino-python.py
@@ -6,21 +6,23 @@ GPIO.setmode(GPIO.BCM)
 pins=[21,20,16,12,7,8,25,24]
 for i in pins:
     GPIO.setup(i, GPIO.OUT, initial=GPIO.LOW)
-
 #arduino = serial.Serial('/dev/ttyACM0', 2304000)
+button_pin = 22
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 while True:
 	i = 0
 	j=0
-	for i in range(0,255):
-		for j in range(0,8):
-			if ( ( (i >> j) & 1 )  == 1 ):
-				print((str(pins[j]) + ",1").encode())
-				#arduino.write((str(pins[j]) + ",1").encode())
-				GPIO.output(pins[j],1)
-			else:
-				print((str(pins[j]) + ",0").encode())
-				#arduino.write((str(pins[j]) + ",0").encode())
-				GPIO.output(pins[j],0)
-			time.sleep(1)
-		print("--------------     ")
-
+    if GPIO.input(button_pin)== GPIO.HIGH:
+    	for i in range(0,255):
+    		for j in range(0,8):
+    			if ( ( (i >> j) & 1 )  == 1 ):
+    				print((str(pins[j]) + ",1").encode())
+    				#arduino.write((str(pins[j]) + ",1").encode())
+    				GPIO.output(pins[j],1)
+    			else:
+    				print((str(pins[j]) + ",0").encode())
+    				#arduino.write((str(pins[j]) + ",0").encode())
+    				GPIO.output(pins[j],0)
+    			time.sleep(1)
+    		print("--------------     ")
